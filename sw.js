2484b94d9557ef87c03afc74951659db4e603e12
FireInspectPro-PBSH <banegas.pe@gmail.com>
2026-08-08 12:14:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    inspector pueda trabajar en un edificio sin señal.
    ============================================================ */
 
-const CACHE_NAME = 'fireinspect-cache-v30';
+const CACHE_NAME = 'fireinspect-cache-v34';
 
 const ARCHIVOS_PARA_CACHEAR = [
   './',
